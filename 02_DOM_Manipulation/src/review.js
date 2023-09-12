b5d39cc139a07d7addf37370937ceabe6949153b
stephenlambertfs <stephenolambert@gmail.com>
2023-09-12 09:50:00 -0600

    updated starter code

--- 02_DOM_Manipulation/src/review.js
+++ 02_DOM_Manipulation/src/review.js
@@ -1,3 +1,30 @@
+//Review variables
+
+
+//Functions & Scope
+
+
+//Anoymous Functions 
+
+//Arrow Functions
+
+
+//Array iterationss
+
+
+//create a list of fruits
+
+
+//test pushing to a list
+
+
+//create a variable that pops an element from the list
+
+//loop through the list with a regular for loop
+
+// loop using a .forEach
+
+//loop using .map
 
 
 
