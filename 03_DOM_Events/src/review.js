c0d6cc1894b8df4682a9bdd2bf3be5f9588bbe72
stephenlambertfs <stephenolambert@gmail.com>
2023-09-13 09:14:55 -0600

    update starter code

---
+++ 03_DOM_Events/src/review.js
@@ -0,0 +1,23 @@
+//Review Arrays
+
+
+//push and pop from an array
+
+
+//destruture an array with spread operator
+
+
+//filter an array
+
+
+//select an element from the dom using querySelector
+
+
+//change the text content
+
+
+//create a new div element
+
+
+//demonstrate appending
+
