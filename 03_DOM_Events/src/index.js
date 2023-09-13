9a9226c8ce1f094ccc645d6d5842949399f346bd
stephenlambertfs <stephenolambert@gmail.com>
2023-09-13 10:39:49 -0600

    updated solution

--- 03_DOM_Events/src/index.js
+++ 03_DOM_Events/src/index.js
@@ -1,154 +1,80 @@
-//BookStore data
-const bookStore = {
-    location: "Seattle",
-    address:'333 st ne Seattle wa 99999',
-    number: 9999999999,
-    name: 'Easley\'s Technical Books',
-    hours: 'Monday - Friday 9am - 6pm',
-    inventory: [
-        {
-            id:1,
-            title: 'Eloquent JavaScript: A Modern Introduction to Programming',
-            author: 'Marjin Haverbeke',
-            price: 10.00,
-            reviews: [{userID: 1, content:'Good book, but not great for new coders'}],
-            inventory: 10,
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-            
-        },
-        {
-            id:2,
-            title: 'JavaScript & JQuery: Interactive Front-End Web Development',
-            author: 'Jon Duckett',
-            price: 45.75,
-            reviews: [{userID: 15, content:'good way to learn JQuery'}],
-            inventory: 2,
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/31SRWF+LkKL._SX398_BO1,204,203,200_.jpg'
-        },
-        {
-            id:3,
-            title: 'JavaScript: The Good Parts',
-            author: 'Douglas Crockford',
-            price: 36.00,
-            reviews: [{userID: 25, content:'I disagree with everything in this book'}, {userID: 250, content:'Only JS book anyone needs'}],
-            inventory: 8,
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-        },
-        {
-            id:4,
-            title: 'JavaScript: The Definitive Guide',
-            author: 'David Flanagan',
-            price: 25.50,
-            reviews: [{userID: 44, content:'Great intro to js book'}, {userID: 350, content:'It really is the Definitive guide'}],
-            inventory: 0,
-            imageUrl: "https://images-na.ssl-images-amazon.com/images/I/51wijnc-Y8L._SX379_BO1,204,203,200_.jpg"
-            
-        },
-        {
-            id:5,
-            title: 'You Don’t Know JS',
-            author: 'Kyle Simpson',
-            price: 6.00,
-            reviews: [{userID: 76, content:'You can find this for free online, no need to pay for it!'}],
-            inventory: 7,
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41T5H8u7fUL._SX331_BO1,204,203,200_.jpg'
-        }, 
-        {
-            id:6,
-            title: 'Learn Enough JavaScript to Be Dangerous',
-            author: 'Michael Hartl',
-            price: 24.00,
-            reviews: [{userID: 50, content:'pretty good'}],
-            inventory: 5,
-            imageUrl: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQyf6xSyTHc7a8mx17ikh6GeTohc88Hn0UgkN-RNF-h4iOwVlkW'
 
-        },
-        {
-            id:7,
-            title: 'Cracking the Coding Interview',
-            author: 'Gayle Laakmann McDowell',
-            price: 49.95,
-            reviews: [{userID: 99, content:'One of the most helpful books for taking on the tech interview'}, {userID:20, content: 'Great but I just wish it was in JavaScript instead of Java' }],
-            inventory: 20,
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg'
 
+document.addEventListener('DOMContentLoaded', () => {
+    function renderHeader(){
+        return document.querySelector('h1').textContent = bookStore.name
+    }
+    
+    console.log(renderHeader())
+    
+    function renderFooter(){
+        const footerDivs = document.querySelectorAll('footer div')
+        console.log(footerDivs)
+        footerDivs[0].textContent = bookStore.name
+        footerDivs[1].textContent = bookStore.address
+        footerDivs[2].textContent = bookStore.hours
+        return footerDivs
+    }
+    console.log(renderFooter())
+    //* 1. use a forEach to iterate over BookStore inventory.
+    //  2. Pass the forEach an anonymous callback that takes the inventory data and creates an li, with an h3 tag, 2 p tags, and image tag.
+    // 3. Add the cardData content to the tags.
+    // 4. Append li to the DOM through the ul with the id of book-list
+    // bookStore.inventory.forEach((cardData) => {
+    //     console.log(cardData)
+    //     const li = document.createElement('li')
+    //     const h3 = document.createElement("h3")
+    //     const pAuthor = document.createElement('p')
+    //     const pPrice = document.createElement('p')
+    //     const image = document.createElement('img')
+    //     const btn = document.createElement('button')
+    //     h3.textContent = cardData.title
+    //     pAuthor.textContent = cardData.author
+    //     pPrice.textContent = `$ ${cardData.price}`
+    //     btn.textContent = "Delete"
+    //     image.src = cardData.imageUrl
+    //     li.className = 'list-li'
+    //     li.append(h3, pAuthor, pPrice, image, btn)
+    //     document.querySelector('#book-list').append(li)
+    // })
+    
+    //* Refactor to make the anonymous callback its own function so it can be reused later. 
+    const renderBookCard = (cardData) => {
+        console.log(cardData)
+        const li = document.createElement('li')
+        const h3 = document.createElement("h3")
+        const pAuthor = document.createElement('p')
+        const pPrice = document.createElement('p')
+        const image = document.createElement('img')
+        const btn = document.createElement('button')
+        h3.textContent = cardData.title
+        pAuthor.textContent = cardData.author
+        pPrice.textContent = `$ ${cardData.price}`
+        btn.textContent = "Delete"
+        image.src = cardData.imageUrl
+        li.className = 'list-li'
+        li.append(h3, pAuthor, pPrice, image, btn)
+        document.querySelector('#book-list').append(li)
+        btn.addEventListener('click', () => li.remove())
+    }
+    bookStore.inventory.forEach(renderBookCard)
+    
+    
+    document.querySelector('label').addEventListener('click',(e)=> console.log(e))
+    
+    function handleForm(e){
+        e.preventDefault()
+        const book = {
+            title: e.target.title.value,
+            author:e.target.author.value,
+            price: e.target.price.value,
+            imageUrl: e.target.imageUrl.value,
+            inventory:e.target.inventory.value,
+            reviews:[]
         }
-    ]
-  }
-
-console.log(document.querySelector('div'))
-console.log(document.querySelector(".list"))
-console.log(document.querySelector("#book-list"))
-console.log(document.getElementById("book-list"))
-console.log(document.querySelectorAll("div"))
-console.log(document.getElementsByClassName('list'))
-console.log(document.getElementsByTagName('li'))
-
-// const div = document.querySelector("div")
-// div.textContent = "Some Text"
-// div.innerText = "Inner Text"
-
-const newDiv = document.createElement("div")
-newDiv.innerText = "New Div Element"
-newDiv.innerHtml = `
-  <p> This is my new div </p>
-`
-function renderHeader(){
-    return document.querySelector('h1').textContent = bookStore.name
-}
-
-console.log(renderHeader())
-
-function renderFooter(){
-    const footerDivs = document.querySelectorAll('footer div')
-    console.log(footerDivs)
-    footerDivs[0].textContent = bookStore.name
-    footerDivs[1].textContent = bookStore.address
-    footerDivs[2].textContent = bookStore.hours
-    return footerDivs
-}
-console.log(renderFooter())
-//* 1. use a forEach to iterate over BookStore inventory.
-//  2. Pass the forEach an anonymous callback that takes the inventory data and creates an li, with an h3 tag, 2 p tags, and image tag.
-// 3. Add the cardData content to the tags.
-// 4. Append li to the DOM through the ul with the id of book-list
-// bookStore.inventory.forEach((cardData) => {
-//     console.log(cardData)
-//     const li = document.createElement('li')
-//     const h3 = document.createElement("h3")
-//     const pAuthor = document.createElement('p')
-//     const pPrice = document.createElement('p')
-//     const image = document.createElement('img')
-//     const btn = document.createElement('button')
-//     h3.textContent = cardData.title
-//     pAuthor.textContent = cardData.author
-//     pPrice.textContent = `$ ${cardData.price}`
-//     btn.textContent = "Delete"
-//     image.src = cardData.imageUrl
-//     li.className = 'list-li'
-//     li.append(h3, pAuthor, pPrice, image, btn)
-//     document.querySelector('#book-list').append(li)
-// })
-
-//* Refactor to make the anonymous callback its own function so it can be reused later. 
-const renderBookCard = (cardData) => {
-    console.log(cardData)
-    const li = document.createElement('li')
-    const h3 = document.createElement("h3")
-    const pAuthor = document.createElement('p')
-    const pPrice = document.createElement('p')
-    const image = document.createElement('img')
-    const btn = document.createElement('button')
-    h3.textContent = cardData.title
-    pAuthor.textContent = cardData.author
-    pPrice.textContent = `$ ${cardData.price}`
-    btn.textContent = "Delete"
-    image.src = cardData.imageUrl
-    li.className = 'list-li'
-    li.append(h3, pAuthor, pPrice, image, btn)
-    document.querySelector('#book-list').append(li)
-}
-bookStore.inventory.forEach(renderBookCard)
-
-
-
+        renderBookCard(book)
+    
+    }
+    document.querySelector('#book-form').addEventListener('submit', handleForm)
+    
+})
--- 03_DOM_Events/src/review.js
+++ 03_DOM_Events/src/review.js
@@ -1,23 +1,71 @@
 //Review Arrays
-
-
+const arr = [10,20,15,0, 1]
 //push and pop from an array
-
+arr.push(25)
+console.log(arr)
+arr.pop()
+console.log(arr)
 
 //destruture an array with spread operator
-
-
+const newArray = [...arr]
+console.log("This is the new Array " + newArray)
 //filter an array
+const filteredArray = arr.filter((num)=> {
+    //console.log(num)
+    //return num > 10
+    if(num < 10){
+        console.log("Invalid Number is ", num)
+        return false
+    }
+    return true
+})
+console.log(filteredArray)
 
+//Review objects
+
+const obj = {
+    key1: {
+        firstname: "Stephen",
+        lastname: "Lambert",
+        func: (data) => console.log(data)
+    },
+    key2: (data)=>console.log(data)
+}
+
+const student = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 22,
+    concentration: "Software Engineering",
+    graduationDate: 2023,
+    courses: ["Javascript", "React", "Python"]
+  };
+
+//use dot notation and brackets to get/change values from the object
+student.firstName = "Jane"
+console.log(student.firstName)
+console.log(student["firstName"])
+console.log(Object.values(student))
+console.log(Object.keys(student))
+//iterate through an object using forLoops and add to the courses array
+
+const studentUpdate = []
+
+for(key in student){
+    console.log(key)
+    console.log(`${key} | ${student[key]}`)
+    studentUpdate.push(key)
+}
+console.log(student)
 
 //select an element from the dom using querySelector
-
-
+const h1 = document.querySelector("#header header div")
+console.log(h1)
 //change the text content
-
-
 //create a new div element
-
-
 //demonstrate appending
-
+const div = document.createElement("div")
+div.textContent = "NEW DIV"
+div.id = "new_id"
+h1.append(div)
+console.log(h1)
