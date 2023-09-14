e12cbe5d3ec0d8ccfe5a9f9d06179081d1d77cf1
stephenlambertfs <stephenolambert@gmail.com>
2023-09-14 10:45:37 -0600

    updated solution

--- 04_Communicating_with_the_Server/src/index.js
+++ 04_Communicating_with_the_Server/src/index.js
@@ -1,19 +1,45 @@
 document.addEventListener("DOMContentLoaded", ()=> {
-    function renderHeader(){
-        return document.querySelector('h1').textContent = bookStore.name
+        
+    // fetch('http://localhost:3000/stores/1')
+    // .then(res => res.json())
+    // .then(store => {
+    //     renderHeader(store)
+    //     renderFooter(store)
+    // })
+    // .catch(e => console.error(e))
+
+    // fetch('http://localhost:3000/books')
+    // .then(res => res.json())
+    // .then(books => books.forEach(renderBookCard))
+    // .catch(e => console.error(e))
+
+    function fetchResource(url){
+        return fetch(url)
+        .then(res => res.json())
+    }
+
+    fetchResource('http://localhost:3000/stores/1')
+        .then(store => {
+            renderHeader(store)
+            renderFooter(store)
+        })
+        .catch(e => console.error(e))
+    
+        fetchResource('http://localhost:3000/books')
+        .then(books => books.forEach(renderBookCard))
+        .catch(e => console.error(e))
+
+    function renderHeader(store){
+        return document.querySelector('h1').textContent = store.name
     }
     
-    console.log(renderHeader())
-    
-    function renderFooter(){
+    function renderFooter(store){
         const footerDivs = document.querySelectorAll('footer div')
-        console.log(footerDivs)
-        footerDivs[0].textContent = bookStore.name
-        footerDivs[1].textContent = bookStore.address
-        footerDivs[2].textContent = bookStore.hours
+        footerDivs[0].textContent = store.name
+        footerDivs[1].textContent = store.address
+        footerDivs[2].textContent = store.hours
         return footerDivs
     }
-    console.log(renderFooter())
     
     const renderBookCard = (cardData) => {
         console.log(cardData)
@@ -33,12 +59,8 @@ document.addEventListener("DOMContentLoaded", ()=> {
         document.querySelector('#book-list').append(li)
         btn.addEventListener('click', (e) => {li.remove()})
     }
-    bookStore.inventory.forEach(renderBookCard)
-    
-    document.querySelector('label').addEventListener('click', (event) => console.log(event))
     
     document.querySelector("#book-form").addEventListener("submit", (e) => {
-        console.log(e)
         e.preventDefault()
         const book = {
             title: e.target.title.value,
@@ -48,8 +70,6 @@ document.addEventListener("DOMContentLoaded", ()=> {
             inventory: e.target.inventory.value,
             reviews: []
         }
-        console.log(book.title)
-        debugger;
         renderBookCard(book)
     })
 })
